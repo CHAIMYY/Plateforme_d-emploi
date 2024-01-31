d08c5c21bd45e66a673d2dda30b4216faba8367c
CHAIMYY <houas.chaimaa@gmail.com>
2024-01-31 11:59:55 +0100

    first commit

---
+++ database/migrations/2024_01_31_095400_create_service_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->string('title');
+            $table->text('desc');
+            $table->string('contact');
+            $table->decimal('cost', 10, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service');
+    }
+};
+
+
+
+
+
+
